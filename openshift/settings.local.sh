9ea877686f7f13bf75338950d7042ac43532bbae
PWEI <patrick.wei@gov.bc.ca>
2019-02-28 10:58:06 -0800

    Initialize project
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>

---
+++ openshift/settings.local.sh
@@ -0,0 +1,2 @@
+export GIT_URI="https://github.com/pwei1018/sbc-pay.git"
+export GIT_REF="openshift"
---
+++ openshift/settings.sh
@@ -0,0 +1,23 @@
+export PROJECT_NAMESPACE="sbc-pay"
+export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshifta}
+
+export GIT_URI="https://github.com/bcgov/sbc-pay.git"
+export GIT_REF="master"
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export -a skip_git_overrides=""
+
+# The project components
+# - They are all  under the main OpenShift folder.
+export components="pay-api pay-web"
+
+# The builds to be triggered after buildconfigs created (not auto-triggered)
+export builds=""
+
+# The images to be tagged after build
+export images="pay-api pay-web"
+
+# The routes for the project
+export routes="pay-web"
