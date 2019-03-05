1d42a5fc7882f78fbf89c7f5074e0ff8575aaafb
PWEI <patrick.wei@gov.bc.ca>
2019-03-05 11:41:11 -0800

    enable tracing
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>

--- pay-api/routes/pay.py
+++ pay-api/routes/pay.py
@@ -1,12 +1,19 @@
 from flask_restplus import Resource, Namespace
 
+import opentracing
+from flask_opentracing import FlaskTracing
+
 api = Namespace('pay', description='Payment System - Pay')
+
+tracer = opentracing.tracer
+tracing = FlaskTracing(tracer)
 
 
 @api.route("")
 class Pay(Resource):
 
     @staticmethod
+    @tracing.trace()
     def get():
         return {"message": "pay"}, 200
 
