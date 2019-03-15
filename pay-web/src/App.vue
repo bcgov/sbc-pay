ab1a73b97cc4bcf161155ec8c4cb102b7e9d06a1
PWEI <patrick.wei@gov.bc.ca>
2019-03-15 14:49:35 -0700

    new web
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>

--- pay-web/src/App.vue
+++ pay-web/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <std-header></std-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import StdHeader from '@/components/StdHeader.vue'
+
 export default {
-  name: 'App'
+  name: 'home',
+  components: {
+    StdHeader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
 </style>
