a6df0164fa16188c650de1585d9cdd356b0a9671
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-04-05 15:57:51 -0700

    Troubleshooting token issue

--- pay-api/service/paybc.py
+++ pay-api/service/paybc.py
@@ -1,7 +1,6 @@
 import base64
 import requests
 import logging
-import json
 
 
 class PayBcService:
@@ -12,7 +11,7 @@ class PayBcService:
 
     def create_invoice(self, invoice_request):
         token_response = self.get_token()
-        token_response = json.loads(token_response)
+        token_response = token_response.json()
         print("Bearer token : {}".format(token_response.get('access_token')))
         logging.info("Bearer token : {}".format(token_response.get('access_token')))
         party = self.create_party(token_response, invoice_request)
@@ -33,7 +32,7 @@ class PayBcService:
 
         party_response = requests.post(party_url, data=party, headers=headers)
         print('party_response : {}'.format(party_response))
-        return json.loads(party_response)
+        return party_response.json()
 
     def create_account(self, token_response, party):
         account_url = self.paybc_base_url + '/cfs/parties/{}/accs'.format(party.get('party_number'))
@@ -48,7 +47,7 @@ class PayBcService:
 
         account_response = requests.post(account_url, data=account, headers=headers)
         print('account_response : {}'.format(account_response))
-        return json.loads(account_response)
+        return account_response.json()
 
     def create_site(self, token_response, account, invoice_request):
         site_url = self.paybc_base_url + '/cfs/parties/{}/accs/sites'.format(account.get('party_number'))
@@ -63,7 +62,7 @@ class PayBcService:
 
         site_response = requests.post(site_url, data=account, headers=headers)
         print('site_response : {}'.format(site_response))
-        return json.loads(site_response)
+        return site_response.json()
 
     def create_contact(self, token_response, site, invoice_request):
         contact_url = self.paybc_base_url + '/cfs/parties/{}/accs/sites/conts'.format(site.get('party_number'))
@@ -81,7 +80,7 @@ class PayBcService:
 
         contact_response = requests.post(contact_url, data=contact, headers=headers)
         print('contact_response : {}'.format(contact_response))
-        return json.loads(contact_response)
+        return contact_response.json()
 
     def create_paybc_invoice(self, token_response, site, invoice_request):
         site_url = self.paybc_base_url + '/cfs/parties/{}/accs/sites'.format(site.get('party_number'))
@@ -96,7 +95,7 @@ class PayBcService:
 
         site_response = requests.post(site_url, data=account, headers=headers)
         print('site_response : {}'.format(site_response))
-        return json.loads(site_response)
+        return site_response.json()
 
     def get_token(self):
         token_url = self.paybc_base_url + '/oauth/token'
@@ -105,11 +104,11 @@ class PayBcService:
         print('Auth Header : {}'.format(basic_auth_encoded))
         logging.info('Auth Header : {}'.format(basic_auth_encoded))
         headers = {
-            "Authorization": "Basic {}".format('bjRWb3p0alNCTmZOV0lpMEtoeHUxZy4uOjJiei1TYzJxNXhtVU85blVPUkZvNmcuLg=='),
+            #"Authorization": "Basic {}".format('bjRWb3p0alNCTmZOV0lpMEtoeHUxZy4uOjJiei1TYzJxNXhtVU85blVPUkZvNmcuLg=='),
             "Content-Type": "application/x-www-form-urlencoded"
         }
 
-        data = "grant_type=client_credentials"
+        data = "grant_type=client_credentials&client_id={}&client_secret={}".format(self.client_id, self.client_secret)
         token_response = requests.post(token_url, data=data, headers=headers)
         print('token_response : {}'.format(token_response.content))
 
