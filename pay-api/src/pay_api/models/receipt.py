c4d615f0bb4a34aae0c9ad7ab71b22f0acd8d49a
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-05-10 11:52:58 -0700

    Payment table changes -- Draft

---
+++ pay-api/src/pay_api/models/receipt.py
@@ -0,0 +1,46 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Model to handle all operations related to Payment Status master data."""
+
+from .db import db, ma
+
+
+class PaymentStatusCode(db.Model):
+    """This class manages all of the base data about a Payment Status Code.
+    """
+
+    __tablename__ = 'payment_state_code'
+
+    status_code = db.Column(db.String(10), primary_key=True)
+    description = db.Column('description', db.String(200), nullable=False)
+
+    @classmethod
+    def find_by_status_code(cls, code):
+        """Given a status code, this will return payment status code details."""
+        payment_status_code = cls.query.filter_by(status_code=code).one_or_none()
+        return payment_status_code
+
+    def save(self):
+        """Save status."""
+        db.session.add(self)
+        db.session.commit()
+
+
+class PaymentStatusCodeSchema(ma.ModelSchema):
+    """Main schema used to serialize the Status Code."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Returns all the fields from the SQLAlchemy class."""
+
+        model = PaymentStatusCode
