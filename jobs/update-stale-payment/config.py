a1e266277b3b5057aef2e5cf31e3469356079fd0
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-08-19 09:36:17 -0700

    Changes for config to PAYBC_CONNECT_TIMEOUT

--- jobs/update-stale-payment/config.py
+++ jobs/update-stale-payment/config.py
@@ -92,7 +92,7 @@ class _Config(object):  # pylint: disable=too-few-public-methods
     PAYBC_PORTAL_URL = os.getenv('PAYBC_PORTAL_URL')
     AUTH_WEB_PAY_TRANSACTION_URL = os.getenv('AUTH_WEB_PAY_TRANSACTION_URL')
     PAYBC_MEMO_LINE_NAME = os.getenv('PAYBC_MEMO_LINE_NAME')
-    CONNECT_TIMEOUT = os.getenv('PAYBC_CONNECT_TIMEOUT', 10)
+    CONNECT_TIMEOUT = int(os.getenv('PAYBC_CONNECT_TIMEOUT', 10))
 
     # BCOL
     BCOL_VERIFY_USER_WSDL_URL = os.getenv('BCOL_VERIFY_USER_WSDL_URL')
