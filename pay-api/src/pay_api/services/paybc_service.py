15cabcb73f4946cde148e7a1827dd8f60e526461
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2019-09-19 13:26:30 -0700

    PROD Fix to limit the account description to 30 characters

--- pay-api/src/pay_api/services/paybc_service.py
+++ pay-api/src/pay_api/services/paybc_service.py
@@ -188,7 +188,7 @@ class PaybcService(PaymentSystemService, OAuthService):
             party.get('party_number', None))
         account: Dict[str, Any] = {
             'party_number': party.get('party_number'),
-            'account_description': party.get('customer_name')
+            'account_description': party.get('customer_name')[:30]
         }
 
         account_response = self.post(account_url, access_token, AuthHeaderType.BEARER, ContentType.JSON, account)
@@ -203,7 +203,7 @@ class PaybcService(PaymentSystemService, OAuthService):
         site: Dict[str, Any] = {
             'party_number': account.get('party_number', None),
             'account_number': account.get('account_number', None),
-            'site_name': party.get('customer_name') + ' Site',
+            'site_name': party.get('customer_name'),
             'city': account_info.get('city', None),
             'address_line_1': account_info.get('address_line_1', None),
             'postal_code': account_info.get('postal_code', None),
