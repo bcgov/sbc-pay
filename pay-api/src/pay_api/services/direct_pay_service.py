dd8e1c7e279726eda65ef682f70db35385b031ae
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-08-06 16:44:37 -0700

    added test cases (#275)
    * updated test cases ; used unquoteplus to properyl un-escape the plus

--- pay-api/src/pay_api/services/direct_pay_service.py
+++ pay-api/src/pay_api/services/direct_pay_service.py
@@ -15,7 +15,7 @@
 import base64
 from datetime import datetime, date
 from typing import Any, Dict
-from urllib.parse import unquote, urlencode
+from urllib.parse import unquote_plus, urlencode
 
 from flask import current_app
 
@@ -58,7 +58,7 @@ class DirectPayService(PaymentSystemService, OAuthService):
 
         url_params = urlencode(url_params_dict)
         # unquote is used below so that unescaped url string can be hashed
-        url_params_dict['hashValue'] = HashingService.encode(unquote(url_params))
+        url_params_dict['hashValue'] = HashingService.encode(unquote_plus(url_params))
         encoded_query_params = urlencode(url_params_dict)  # encode it again to inlcude the hash
         paybc_url = current_app.config.get('PAYBC_DIRECT_PAY_PORTAL_URL')
         return f'{paybc_url}?{encoded_query_params}'
