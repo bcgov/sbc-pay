aaaca6e3e28efefd2a7d1a73f75dc3bccbba1b73
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2020-09-23 11:21:10 -0700

    Fixing issue with migration

--- pay-api/migrations/versions/1283463d3b4a_new_payment_method_tables.py
+++ pay-api/migrations/versions/1283463d3b4a_new_payment_method_tables.py
@@ -85,19 +85,31 @@ def upgrade():
                     sa.ForeignKeyConstraint(['invoice_id'], ['invoice.id'], ),
                     sa.PrimaryKeyConstraint('id')
                     )
-    op.drop_index('ix_internal_payment_account_account_id', table_name='internal_payment_account')
-    op.drop_table('internal_payment_account')
-    op.drop_index('ix_credit_payment_account_account_id', table_name='credit_payment_account')
-    op.drop_index('ix_credit_payment_account_paybc_account', table_name='credit_payment_account')
-    op.drop_table('credit_payment_account')
-    op.drop_index('ix_bcol_payment_account_account_id', table_name='bcol_payment_account')
-    op.drop_index('ix_bcol_payment_account_bcol_account_id', table_name='bcol_payment_account')
-    op.drop_index('ix_bcol_payment_account_bcol_user_id', table_name='bcol_payment_account')
-    op.drop_table('bcol_payment_account')
+    # op.drop_index('ix_internal_payment_account_account_id', table_name='internal_payment_account')
+    # op.drop_table('internal_payment_account')
+    # op.drop_index('ix_credit_payment_account_account_id', table_name='credit_payment_account')
+    # op.drop_index('ix_credit_payment_account_paybc_account', table_name='credit_payment_account')
+    # op.drop_table('credit_payment_account')
+    # op.drop_index('ix_bcol_payment_account_account_id', table_name='bcol_payment_account')
+    # op.drop_index('ix_bcol_payment_account_bcol_account_id', table_name='bcol_payment_account')
+    # op.drop_index('ix_bcol_payment_account_bcol_user_id', table_name='bcol_payment_account')
+    # op.drop_table('bcol_payment_account')
     op.add_column('cfs_account', sa.Column('contact_party', sa.String(length=50), nullable=True))
     op.add_column('cfs_account', sa.Column('payment_instrument_number', sa.String(length=50), nullable=True))
     op.add_column('invoice', sa.Column('cfs_account_id', sa.Integer(), nullable=True))
-    op.create_foreign_key(None, 'invoice', 'cfs_account', ['cfs_account_id'], ['id'])
+
+    # Update invoice which have credit_account_id with matching details from cfs_account
+    conn = op.get_bind()
+    res = conn.execute("select cfs.id, credit.id from cfs_account cfs, credit_payment_account credit "
+                       "where credit.paybc_account=cfs.cfs_account and credit.account_id=cfs.account_id;")
+    cfs_accounts = res.fetchall()
+    for cfs_account in cfs_accounts:
+        cfs_id = cfs_account[0]
+        credit_id = cfs_account[1]
+
+        op.execute(f"update invoice set cfs_account_id='{cfs_id}' where credit_account_id='{credit_id}'")
+
+    op.create_foreign_key('fk_cfs_account_id', 'invoice', 'cfs_account', ['cfs_account_id'], ['id'])
     op.add_column('payment_account', sa.Column('billable', sa.Boolean(), nullable=True))
     op.add_column('payment_account', sa.Column('credit', sa.Float(), nullable=True))
     op.add_column('payment_account', sa.Column('running_balance', sa.Float(), nullable=True))
@@ -109,52 +121,52 @@ def downgrade():
     op.drop_column('payment_account', 'running_balance')
     op.drop_column('payment_account', 'credit')
     op.drop_column('payment_account', 'billable')
-    op.drop_constraint(None, 'invoice', type_='foreignkey')
+    op.drop_constraint('fk_cfs_account_id', 'invoice', type_='foreignkey')
     op.drop_column('invoice', 'cfs_account_id')
     op.drop_column('cfs_account', 'payment_instrument_number')
     op.drop_column('cfs_account', 'contact_party')
-    op.create_table('bcol_payment_account',
-                    sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-                    sa.Column('bcol_user_id', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
-                    sa.Column('bcol_account_id', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
-                    sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
-                    sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
-                                            name='bcol_payment_account_account_id_fkey'),
-                    sa.PrimaryKeyConstraint('id', name='bcol_payment_account_pkey')
-                    )
-    op.create_index('ix_bcol_payment_account_bcol_user_id', 'bcol_payment_account', ['bcol_user_id'], unique=False)
-    op.create_index('ix_bcol_payment_account_bcol_account_id', 'bcol_payment_account', ['bcol_account_id'],
-                    unique=False)
-    op.create_index('ix_bcol_payment_account_account_id', 'bcol_payment_account', ['account_id'], unique=False)
-    op.create_table('credit_payment_account',
-                    sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-                    sa.Column('corp_number', sa.VARCHAR(length=20), autoincrement=False, nullable=True),
-                    sa.Column('corp_type_code', sa.VARCHAR(length=10), autoincrement=False, nullable=True),
-                    sa.Column('paybc_account', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
-                    sa.Column('paybc_party', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
-                    sa.Column('paybc_site', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
-                    sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
-                    sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
-                                            name='credit_payment_account_account_id_fkey'),
-                    sa.ForeignKeyConstraint(['corp_type_code'], ['corp_type.code'],
-                                            name='credit_payment_account_corp_type_code_fkey'),
-                    sa.PrimaryKeyConstraint('id', name='credit_payment_account_pkey')
-                    )
-    op.create_index('ix_credit_payment_account_paybc_account', 'credit_payment_account', ['paybc_account'],
-                    unique=False)
-    op.create_index('ix_credit_payment_account_account_id', 'credit_payment_account', ['account_id'], unique=False)
-    op.create_table('internal_payment_account',
-                    sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-                    sa.Column('corp_number', sa.VARCHAR(length=20), autoincrement=False, nullable=True),
-                    sa.Column('corp_type_code', sa.VARCHAR(length=10), autoincrement=False, nullable=True),
-                    sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
-                    sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
-                                            name='internal_payment_account_account_id_fkey'),
-                    sa.ForeignKeyConstraint(['corp_type_code'], ['corp_type.code'],
-                                            name='internal_payment_account_corp_type_code_fkey'),
-                    sa.PrimaryKeyConstraint('id', name='internal_payment_account_pkey')
-                    )
-    op.create_index('ix_internal_payment_account_account_id', 'internal_payment_account', ['account_id'], unique=False)
+    # op.create_table('bcol_payment_account',
+    #                 sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+    #                 sa.Column('bcol_user_id', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
+    #                 sa.Column('bcol_account_id', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
+    #                 sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
+    #                 sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
+    #                                         name='bcol_payment_account_account_id_fkey'),
+    #                 sa.PrimaryKeyConstraint('id', name='bcol_payment_account_pkey')
+    #                 )
+    # op.create_index('ix_bcol_payment_account_bcol_user_id', 'bcol_payment_account', ['bcol_user_id'], unique=False)
+    # op.create_index('ix_bcol_payment_account_bcol_account_id', 'bcol_payment_account', ['bcol_account_id'],
+    #                 unique=False)
+    # op.create_index('ix_bcol_payment_account_account_id', 'bcol_payment_account', ['account_id'], unique=False)
+    # op.create_table('credit_payment_account',
+    #                 sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+    #                 sa.Column('corp_number', sa.VARCHAR(length=20), autoincrement=False, nullable=True),
+    #                 sa.Column('corp_type_code', sa.VARCHAR(length=10), autoincrement=False, nullable=True),
+    #                 sa.Column('paybc_account', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
+    #                 sa.Column('paybc_party', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
+    #                 sa.Column('paybc_site', sa.VARCHAR(length=50), autoincrement=False, nullable=True),
+    #                 sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
+    #                 sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
+    #                                         name='credit_payment_account_account_id_fkey'),
+    #                 sa.ForeignKeyConstraint(['corp_type_code'], ['corp_type.code'],
+    #                                         name='credit_payment_account_corp_type_code_fkey'),
+    #                 sa.PrimaryKeyConstraint('id', name='credit_payment_account_pkey')
+    #                 )
+    # op.create_index('ix_credit_payment_account_paybc_account', 'credit_payment_account', ['paybc_account'],
+    #                 unique=False)
+    # op.create_index('ix_credit_payment_account_account_id', 'credit_payment_account', ['account_id'], unique=False)
+    # op.create_table('internal_payment_account',
+    #                 sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+    #                 sa.Column('corp_number', sa.VARCHAR(length=20), autoincrement=False, nullable=True),
+    #                 sa.Column('corp_type_code', sa.VARCHAR(length=10), autoincrement=False, nullable=True),
+    #                 sa.Column('account_id', sa.INTEGER(), autoincrement=False, nullable=True),
+    #                 sa.ForeignKeyConstraint(['account_id'], ['payment_account.id'],
+    #                                         name='internal_payment_account_account_id_fkey'),
+    #                 sa.ForeignKeyConstraint(['corp_type_code'], ['corp_type.code'],
+    #                                         name='internal_payment_account_corp_type_code_fkey'),
+    #                 sa.PrimaryKeyConstraint('id', name='internal_payment_account_pkey')
+    #                 )
+    # op.create_index('ix_internal_payment_account_account_id', 'internal_payment_account', ['account_id'], unique=False)
     op.drop_table('invoice_batch_link')
     op.drop_table('ejv_batch_link')
     op.drop_table('daily_payment_batch_link')
