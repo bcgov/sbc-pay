c3f962a5461c31ffef5c092ac6d06ac69b84026a
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-10-26 15:09:34 -0700

    Example/tests added for bank account endpoints

--- pay-api/tests/unit/api/test_bank_accounts.py
+++ pay-api/tests/unit/api/test_bank_accounts.py
@@ -29,7 +29,7 @@ def test_bank_account_valid_bank(session, client, jwt, app):
     headers = {'Authorization': f'Bearer {token}', 'content-type': 'application/json'}
 
     valid_bank_details = {
-        'bankInstitutionNumber': '2001',
+        'bankInstitutionNumber': '0001',
         'bankTransitNumber': '00720',
         'bankAccountNumber': '1234567',
 
@@ -38,4 +38,45 @@ def test_bank_account_valid_bank(session, client, jwt, app):
     rv = client.post('/api/v1/bank-accounts/verifications', data=json.dumps(valid_bank_details),
                      headers=headers)
     assert rv.status_code == 200
-    print(rv.json)
+    assert rv.json.get('isValid') is True
+
+
+def test_bank_account_invalid_bank_one_error(session, client, jwt, app):
+    """Assert that the endpoint returns 200."""
+    token = jwt.create_jwt(get_claims(), token_header)
+    headers = {'Authorization': f'Bearer {token}', 'content-type': 'application/json'}
+
+    valid_bank_details = {
+        'bankInstitutionNumber': '0002',
+        'bankTransitNumber': '00720',
+        'bankAccountNumber': '1234567',
+
+    }
+
+    rv = client.post('/api/v1/bank-accounts/verifications', data=json.dumps(valid_bank_details),
+                     headers=headers)
+    assert rv.status_code == 200
+    assert rv.json.get('isValid') is False
+    assert rv.json.get('message')[0] == 'Bank Number is Invalid.'
+
+
+def test_bank_account_invalid_bank_multiple_error(session, client, jwt, app):
+    """Assert that the endpoint returns 200."""
+    token = jwt.create_jwt(get_claims(), token_header)
+    headers = {'Authorization': f'Bearer {token}', 'content-type': 'application/json'}
+
+    valid_bank_details = {
+        'bankInstitutionNumber': '0003',
+        'bankTransitNumber': '00720',
+        'bankAccountNumber': '1234567',
+
+    }
+
+    rv = client.post('/api/v1/bank-accounts/verifications', data=json.dumps(valid_bank_details),
+                     headers=headers)
+    assert rv.status_code == 200
+    assert rv.json.get('isValid') is False
+    assert len(rv.json.get('message')) == 3
+    assert 'Account number has invalid characters.' in rv.json.get('message')
+    assert 'Account number has non-numeric characters.' in rv.json.get('message')
+    assert 'Account number length is not valid for this bank.' in rv.json.get('message')
