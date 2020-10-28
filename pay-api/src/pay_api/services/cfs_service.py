6ac823b3be2e5479eca1ddf6bda63826853720cb
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-10-28 06:41:34 -0700

    Fixed all errors

--- pay-api/src/pay_api/services/cfs_service.py
+++ pay-api/src/pay_api/services/cfs_service.py
@@ -83,12 +83,14 @@ class CFSService(OAuthService):
                         bank_validation_response.get('CAS-Returned-Messages'))
                 }
             else:
+                current_app.logger.debug('<Bank validation HTTP exception- {}', bank_validation_response_obj.text)
                 validation_response = {
                     'status_code': bank_validation_response_obj.status_code,
                     'msessage': 'Bank validation service cant be reached'
                 }
 
         except ServiceUnavailableException as exc:  # suppress all other errors
+            current_app.logger.debug('<Bank validation ServiceUnavailableException exception- {}', exc.error)
             validation_response = {
                 'status_code': HTTPStatus.SERVICE_UNAVAILABLE.value,
                 'msessage': exc.error
