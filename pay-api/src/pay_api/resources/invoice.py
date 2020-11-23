1452ca4832158df2488263133cf96fe066842aa8
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2020-11-23 12:17:22 -0800

    4857: Addin account payments endpoint to return all failed payments for NSF

--- pay-api/src/pay_api/resources/invoice.py
+++ pay-api/src/pay_api/resources/invoice.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Resource for Payment endpoints."""
+"""Resource for Payment Request/Invoice endpoints."""
 from http import HTTPStatus
 
 from flask import current_app, jsonify, request
