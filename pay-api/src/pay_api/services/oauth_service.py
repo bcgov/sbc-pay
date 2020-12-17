4927b1a1d7d945c7093932917b5bdc66ac635d71
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-12-16 21:58:04 -0800

    fixing the order of the param

--- pay-api/src/pay_api/services/oauth_service.py
+++ pay-api/src/pay_api/services/oauth_service.py
@@ -93,7 +93,7 @@ class OAuthService:
     @staticmethod
     def get(endpoint, token, auth_header_type: AuthHeaderType,  # pylint:disable=too-many-arguments
             content_type: ContentType,
-            retry_on_failure: bool = False, return_none_if_404: bool = False, additional_headers: Dict = None ):
+            retry_on_failure: bool = False, return_none_if_404: bool = False, additional_headers: Dict = None):
         """GET service."""
         current_app.logger.debug('<GET')
 
