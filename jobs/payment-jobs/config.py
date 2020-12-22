6d5dcf768f0a38f918393eb49fec92c5bc6a0944
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-12-22 14:02:07 -0800

    Adding missing config details for mailer

--- jobs/payment-jobs/config.py
+++ jobs/payment-jobs/config.py
@@ -135,6 +135,9 @@ class _Config(object):  # pylint: disable=too-few-public-methods
     DEBUG = True
     PAD_CONFIRMATION_PERIOD_IN_DAYS = int(os.getenv('PAD_CONFIRMATION_PERIOD_IN_DAYS', '3'))
 
+    NATS_MAILER_CLIENT_NAME = _get_config('NATS_MAILER_CLIENT_NAME', default='account.mailer.worker')
+    NATS_MAILER_SUBJECT = _get_config('NATS_MAILER_SUBJECT', default='account.mailer')
+
 
 class DevConfig(_Config):  # pylint: disable=too-few-public-methods
     TESTING = False
