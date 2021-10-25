02f5e09a9759338fff60a907fe57c9a367c79bd1
karthik-aot <karthik.r@aot-technologies.com>
2021-10-25 11:26:06 -0700

    merge heads

---
+++ pay-api/migrations/versions/bee492fa7640_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: bee492fa7640
+Revises: 4cb0dc8e0013, db40bb37a30c
+Create Date: 2021-10-25 11:25:12.566463
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bee492fa7640'
+down_revision = ('4cb0dc8e0013', 'db40bb37a30c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
