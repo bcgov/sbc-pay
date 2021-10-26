86bf7a39720a81278437d08cf034d5f6ff7102dc
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2021-10-26 10:45:17 -0700

    Feature/9550 backend integration changes (#803)

--- pay-api/src/pay_api/models/comment.py
+++ pay-api/src/pay_api/models/comment.py
@@ -70,3 +70,4 @@ class CommentSchema(BaseSchema):  # pylint: disable=too-many-ancestors
         exclude = ['routing_slip']
 
     routing_slip_number = fields.String(data_key='routing_slip_number')
+    submitter_name = fields.String(data_key="submitter_display_name")
