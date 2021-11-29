dc2991190c2d43d4355e2594ab2f931c144c51dc
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-11-29 11:43:05 -0800

    fixed ci

--- jobs/payment-jobs/tasks/cfs_create_invoice_task.py
+++ jobs/payment-jobs/tasks/cfs_create_invoice_task.py
@@ -77,8 +77,9 @@ class CreateInvoiceTask:  # pylint:disable=too-few-public-methods
                 routing_slip.payment_account_id)
             cfs_account: CfsAccountModel = CfsAccountModel.find_effective_by_account_id(
                 routing_slip_payment_account.id)
-            invoice_reference = InvoiceReferenceModel.find_reference_by_invoice_id_and_status(invoice.id,
-                                                                                              status_code=InvoiceReferenceStatus.ACTIVE.value)
+            invoice_reference = InvoiceReferenceModel. \
+                find_reference_by_invoice_id_and_status(invoice.id,
+                                                        status_code=InvoiceReferenceStatus.ACTIVE.value)
             try:
                 # find receipts against the invoice and unapply
                 # apply receipt now
