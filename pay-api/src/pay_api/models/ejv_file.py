5a2e73258b24724734ed461a28d05c624d97a56e
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2021-12-20 12:40:30 -0800

    Changes for creating AP file for refund.

--- pay-api/src/pay_api/models/ejv_file.py
+++ pay-api/src/pay_api/models/ejv_file.py
@@ -30,7 +30,7 @@ class EjvFile(BaseModel):  # pylint: disable=too-many-instance-attributes
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created_on = db.Column('created_on', db.DateTime, nullable=False, default=datetime.now)
     completed_on = db.Column('completed_on', db.DateTime, nullable=True)
-    file_type = db.Column('file_type', db.String, nullable=True, default=EjvFileType.PAYMENT.value)
+    file_type = db.Column('file_type', db.String, nullable=True, default=EjvFileType.DISBURSEMENT.value)
     file_ref = db.Column('file_ref', db.String, nullable=False, index=True)
     disbursement_status_code = db.Column(db.String(20), ForeignKey('disbursement_status_codes.code'), nullable=True)
     message = db.Column('message', db.String, nullable=True, index=False)
