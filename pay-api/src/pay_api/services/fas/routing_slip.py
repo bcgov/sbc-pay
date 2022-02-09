04994f1dcc5e2c7c6822d63c3dd6690f44f1a755
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2022-02-09 13:42:47 -0800

    10507 - Daily RS report fix for total USD amount  display (#888)

--- pay-api/src/pay_api/services/fas/routing_slip.py
+++ pay-api/src/pay_api/services/fas/routing_slip.py
@@ -213,12 +213,12 @@ class RoutingSlip:  # pylint: disable=too-many-instance-attributes, too-many-pub
                 no_of_cash += 1
                 # TODO check if the payment is CAD or USD.
                 total_cash_cad += float(routing_slip.total)
-                if routing_slip.total_usd:
+                if routing_slip.total_usd is not None:
                     total_cash_usd += float(routing_slip.total_usd)
             else:
                 no_of_cheque += 1
                 total_cheque_cad += float(routing_slip.total)
-                if routing_slip.total_usd:
+                if routing_slip.total_usd is not None:
                     total_cheque_usd += float(routing_slip.total_usd)
 
         report_dict = dict(
