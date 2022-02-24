24684152a3e49a246beeb14522f1bd22be077009
karthik-aot <78374865+karthik-aot@users.noreply.github.com>
2022-02-23 18:33:33 -0800

    Feature/11184 (#895)

--- pay-api/src/pay_api/services/fas/routing_slip_status_transition_service.py
+++ pay-api/src/pay_api/services/fas/routing_slip_status_transition_service.py
@@ -64,10 +64,15 @@ class RoutingSlipStatusTransitionService:  # pylint: disable=too-many-instance-a
         """Return all the status transition available."""
         transition_list: List[RoutingSlipStatus] = RoutingSlipStatusTransitionService.STATUS_TRANSITIONS.get(
             rs_model.status, [])
+
         if RoutingSlipStatus.REFUND_AUTHORIZED.value in transition_list:
             # self approval not permitted
             refund_model = RefundModel.find_by_routing_slip_id(rs_model.id)
             is_same_user = refund_model.requested_by == kwargs['user'].user_name
+            if is_same_user:
+                return transition_list[1:]
+        elif RoutingSlipStatus.WRITE_OFF_AUTHORIZED.value in transition_list:
+            is_same_user = rs_model.updated_by == kwargs['user'].user_name
             if is_same_user:
                 return transition_list[1:]
 
