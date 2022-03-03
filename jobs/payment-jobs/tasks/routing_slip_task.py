c04738262e0fa201f3cc6830f154ef470fe5e7ce
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2022-03-02 19:36:58 -0800

    fixing NSF balance bug

--- jobs/payment-jobs/tasks/routing_slip_task.py
+++ jobs/payment-jobs/tasks/routing_slip_task.py
@@ -161,7 +161,6 @@ class RoutingSlipTask:  # pylint:disable=too-few-public-methods
 
                 inv = cls._create_nsf_invoice(cfs_account, routing_slip.number, payment_account)
                 # Reduce the NSF fee from remaining amount.
-                current_app.logger.debug(f'Reducing NSF amount  From {routing_slip.number} : Remaining Amount {routing_slip.remaining_amount} Inv.total : {inv.total}')
                 routing_slip.remaining_amount = float(routing_slip.remaining_amount) - inv.total
                 routing_slip.save()
 
