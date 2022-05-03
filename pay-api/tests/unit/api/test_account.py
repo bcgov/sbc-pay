e8e1455df5979214029a014fcc3fa28e8fce4caa
Ritvick Paliwal <ritvick.paliwal@gmail.com>
2022-05-03 11:46:19 -0400

    added unit tests

--- pay-api/tests/unit/api/test_account.py
+++ pay-api/tests/unit/api/test_account.py
@@ -31,8 +31,8 @@ from pay_api.schemas import utils as schema_utils
 from pay_api.utils.enums import CfsAccountStatus, PaymentMethod, Role
 from tests.utilities.base_test import (
     get_basic_account_payload, get_claims, get_gov_account_payload, get_gov_account_payload_with_no_revenue_account,
-    get_pad_account_payload, get_payment_request, get_payment_request_with_service_fees, get_premium_account_payload,
-    get_unlinked_pad_account_payload, token_header)
+    get_pad_account_payload, get_payment_request, get_payment_request_for_cso, get_payment_request_with_service_fees,
+    get_premium_account_payload, get_unlinked_pad_account_payload, token_header)
 
 
 fake = Faker()
