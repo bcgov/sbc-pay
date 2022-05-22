e0c1ca7e50542fe71eac3ffa240a7a1e617fbe03
Travis Semple <travis8814@gmail.com>
2022-05-22 13:47:14 -0700

    Lint error.

--- pay-api/tests/unit/services/test_payment.py
+++ pay-api/tests/unit/services/test_payment.py
@@ -88,7 +88,7 @@ def test_search_payment_history(session):
     # Check for invoice status.
     assert results.get('items')[0]['status_code'] == InvoiceStatus.CREATED.value
     assert results.get('items')[0]['status_code_description'] == 'Pending'
-    
+
     # Add one more payment
     payment_account.save()
     invoice = factory_invoice(payment_account)
