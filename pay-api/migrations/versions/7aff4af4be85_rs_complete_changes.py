bc9bc3bc67522cdd2b074c30c00124b0b06948d3
Travis Semple <travis8814@gmail.com>
2022-05-30 14:25:34 -0700

    Dependency update + migration.

---
+++ pay-api/migrations/versions/7aff4af4be85_rs_complete_changes.py
@@ -0,0 +1,42 @@
+"""add details for RS_CANT_LINK_COMPLETE
+
+Revision ID: 7aff4af4be85
+Revises: 6a6b042b831a
+Create Date: 2022-05-30 08:23:00.535893
+
+"""
+from alembic import op
+from sqlalchemy import String
+from sqlalchemy.sql import column, table
+
+
+# revision identifiers, used by Alembic.
+revision = '7aff4af4be85'
+down_revision = '6a6b042b831a'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    error_code_table = table('error_codes',
+                             column('code', String),
+                             column('title', String),
+                             column('detail', String)
+                             )
+
+    op.bulk_insert(
+        error_code_table,
+        [
+            {
+                'code': 'RS_CANT_LINK_COMPLETE',
+                'title': 'Routing Slip cannot be linked.',
+                'detail': 'Linking cannot be performed since the routing slip is Complete.'
+            }
+        ])
+
+    op.execute("UPDATE routing_slips set status = 'COMPLETE' where remaining_amount < 0.01 and status = 'ACTIVE'")
+
+
+def downgrade():
+    op.execute("DELETE FROM error_codes where code in ('RS_CANT_LINK_COMPLETE')")
+    op.execute("UPDATE routing_slips set status = 'ACTIVE' where remaining_amount < 0.01 and status = 'COMPLETE'")
+
