743ae12fe421604e80f8e2c046a7e4edcfc7dee5
Jia Xu <Jia.Xu@gov.bc.ca>
2023-02-24 08:23:49 -0800

    add migration of filling for payment/refund date

---
+++ pay-api/migrations/versions/2023_02_23_d2d9864164d1_update_refund_payment_date_to_invoices.py
@@ -0,0 +1,49 @@
+"""update refund payment date to invoices
+
+Revision ID: d2d9864164d1
+Revises: 0a3ad7c041f6
+Create Date: 2023-02-23 10:14:27.347074
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'd2d9864164d1'
+down_revision = '0a3ad7c041f6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Update payment and refund date in invoices table."""
+    # 1. Find all id and payment date from invoice table linking with invoice_reference table.
+    # 2. Update payment_date with invoice_id.
+    conn = op.get_bind()
+    res = conn.execute(f'select i.id as "id", p.payment_date as "date"\
+                                from invoice_references ir \
+                                inner join invoices i on i.id = ir.invoice_id \
+                                inner join payments p on p.invoice_number = ir.invoice_number \
+                                where p.payment_date IS NOT null AND i.payment_date IS null;')
+    payments = res.fetchall()
+    for payment in payments:
+        invoice_id = payment[0]
+        payment_date = payment[1]
+        op.execute(f"update invoices set payment_date=\'{payment_date}\' where id = {invoice_id}")
+
+    # 1. Find all id and refund date from invoice table linking with refunds table.
+    # 2. Update refund_date with invoice_id.
+    res = conn.execute(f'select i.id as "id", r.requested_date as "date"\
+                                    from invoices i \
+                                    inner join refunds r on r.invoice_id = i.id\
+                                    where i.invoice_status_code = \'REFUNDED\'\
+                                    and i.refund_date is null;')
+    refunds = res.fetchall()
+    for refund in refunds:
+        id = refund[0]
+        refund_date = refund[1]
+        op.execute(f"update invoices set refund_date=\'{refund_date}\' where id = {id}")
+
+
+def downgrade():
+    pass
