90fd611757a2ef633f22575355fc5f731ae6ad80
Travis Semple <travis8814@gmail.com>
2023-02-25 14:59:26 -0800

    Set timeout to 90s, up from original 60s.

--- pay-api/docker-entrypoint.sh
+++ pay-api/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo 'starting application'
-gunicorn --bind 0.0.0.0:8080 --timeout 360 --workers 3  wsgi:application
+gunicorn --bind 0.0.0.0:8080 --timeout 90 --workers 3  wsgi:application
