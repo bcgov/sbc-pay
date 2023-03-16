d4aee9a5032a12db21b1e8f48c6aed29571db4c4
Travis Semple <travis8814@gmail.com>
2023-03-16 14:05:37 -0700

    Logging fix part 2

--- jobs/payment-jobs/tasks/statement_task.py
+++ jobs/payment-jobs/tasks/statement_task.py
@@ -73,7 +73,7 @@ class StatementTask:  # pylint:disable=too-few-public-methods
                 'endDate': previous_day.strftime('%Y-%m-%d')
             }
         }
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
     def _generate_weekly_statements(cls, target_time: datetime):
@@ -90,7 +90,7 @@ class StatementTask:  # pylint:disable=too-few-public-methods
             }
         }
 
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
     def _generate_monthly_statements(cls, target_time: datetime):
@@ -107,16 +107,19 @@ class StatementTask:  # pylint:disable=too-few-public-methods
             }
         }
 
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
-    def _create_statement_records(cls, previous_day, search_filter, statement_settings):
+    def _create_statement_records(cls, search_filter, statement_settings):
         statement_from = None
         statement_to = None
         if search_filter.get('dateFilter', None):
             statement_from = parse(search_filter.get('dateFilter').get('startDate'))
             statement_to = parse(search_filter.get('dateFilter').get('endDate'))
-            current_app.logger.debug(f'Statements for range: {statement_from.date()} to {statement_to.date()}')
+            if statement_from == statement_to:
+                current_app.logger.debug(f'Statements for day: {statement_from.date()}')
+            else:
+                current_app.logger.debug(f'Statements for week: {statement_from.date()} to {statement_to.date()}')
         elif search_filter.get('monthFilter', None):
             statement_from, statement_to = get_first_and_last_dates_of_month(
                 search_filter.get('monthFilter').get('month'), search_filter.get('monthFilter').get('year'))
