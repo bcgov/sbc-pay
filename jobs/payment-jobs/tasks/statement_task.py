fbc8eb2609dce2ef6f420a9f6ffa350570cbac4e
Travis Semple <travis8814@gmail.com>
2023-03-16 16:33:42 -0700

    Statement weekly fix (#1144)
    * Lol, use logic that's consistent with logging.
    
    * Fix for weekly statements.
    
    * Fix logging.
    
    * Logging fix part 2

--- jobs/payment-jobs/tasks/statement_task.py
+++ jobs/payment-jobs/tasks/statement_task.py
@@ -73,7 +73,7 @@ class StatementTask:  # pylint:disable=too-few-public-methods
                 'endDate': previous_day.strftime('%Y-%m-%d')
             }
         }
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
     def _generate_weekly_statements(cls, target_time: datetime):
@@ -82,13 +82,15 @@ class StatementTask:  # pylint:disable=too-few-public-methods
         statement_settings = StatementSettingsModel.find_accounts_settings_by_frequency(previous_day,
                                                                                         StatementFrequency.WEEKLY)
         current_app.logger.debug(f'Found {len(statement_settings)} accounts to generate WEEKLY statements')
+        statement_from, statement_to = get_week_start_and_end_date(previous_day, index=1)
         search_filter = {
-            'weekFilter': {
-                'index': 1  # previous week
+            'dateFilter': {
+                'startDate': statement_from.strftime('%Y-%m-%d'),
+                'endDate': statement_to.strftime('%Y-%m-%d')
             }
         }
 
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
     def _generate_monthly_statements(cls, target_time: datetime):
@@ -105,20 +107,19 @@ class StatementTask:  # pylint:disable=too-few-public-methods
             }
         }
 
-        cls._create_statement_records(previous_day, search_filter, statement_settings)
+        cls._create_statement_records(search_filter, statement_settings)
 
     @classmethod
-    def _create_statement_records(cls, previous_day, search_filter, statement_settings):
+    def _create_statement_records(cls, search_filter, statement_settings):
         statement_from = None
         statement_to = None
         if search_filter.get('dateFilter', None):
             statement_from = parse(search_filter.get('dateFilter').get('startDate'))
             statement_to = parse(search_filter.get('dateFilter').get('endDate'))
-            current_app.logger.debug(f'Statements for day: {statement_from.date()}')
-        elif search_filter.get('weekFilter', None):
-            index = search_filter.get('weekFilter').get('index')
-            statement_from, statement_to = get_week_start_and_end_date(target_date=previous_day, index=index)
-            current_app.logger.debug(f'Statements for week: {statement_from.date()} to {statement_to.date()}')
+            if statement_from == statement_to:
+                current_app.logger.debug(f'Statements for day: {statement_from.date()}')
+            else:
+                current_app.logger.debug(f'Statements for week: {statement_from.date()} to {statement_to.date()}')
         elif search_filter.get('monthFilter', None):
             statement_from, statement_to = get_first_and_last_dates_of_month(
                 search_filter.get('monthFilter').get('month'), search_filter.get('monthFilter').get('year'))
