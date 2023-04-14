4928407b2c8a05207a2db3091d9b912c19a11bdf
Travis Semple <travis8814@gmail.com>
2023-04-14 12:13:47 -0700

    Clean up old LD flags, unused.

--- pay-api/src/pay_api/services/base_payment_system.py
+++ pay-api/src/pay_api/services/base_payment_system.py
@@ -30,7 +30,6 @@ from pay_api.models import PaymentLineItem as PaymentLineItemModel
 from pay_api.models import PaymentTransaction as PaymentTransactionModel
 from pay_api.models import Receipt as ReceiptModel
 from pay_api.services.cfs_service import CFSService
-from pay_api.services.flags import flags
 from pay_api.services.invoice import Invoice
 from pay_api.services.invoice_reference import InvoiceReference
 from pay_api.services.payment import Payment
@@ -162,7 +161,7 @@ class PaymentSystemService(ABC):  # pylint: disable=too-many-instance-attributes
         if invoice.invoice_status_code == InvoiceStatus.APPROVED.value \
                 and InvoiceReferenceModel.find_by_invoice_id_and_status(
                     invoice.id, InvoiceReferenceStatus.ACTIVE.value) is None:
-            return InvoiceStatus.CANCELLED.value if flags.is_on('NEW_REFUNDED_STATUSES', default=False) else None
+            return InvoiceStatus.CANCELLED.value
 
         cfs_account: CfsAccountModel = CfsAccountModel.find_effective_by_account_id(invoice.payment_account_id)
         if cfs_account is None:
@@ -191,7 +190,7 @@ class PaymentSystemService(ABC):  # pylint: disable=too-many-instance-attributes
         current_app.logger.info(
             f'Updating credit amount to  {payment_account.credit} for account {payment_account.auth_account_id}')
         payment_account.flush()
-        return InvoiceStatus.CREDITED.value if flags.is_on('NEW_REFUNDED_STATUSES', default=False) else None
+        return InvoiceStatus.CREDITED.value
 
     @staticmethod
     def _publish_refund_to_mailer(invoice: InvoiceModel):
