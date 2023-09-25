fbe0393ae8fe0b98fc5458f3de9865ddb6a0f7c6
Travis Semple <travis8814@gmail.com>
2023-09-25 14:20:01 -0700

    A start on some of the database model changes.

--- pay-api/src/pay_api/resources/v1/__init__.py
+++ pay-api/src/pay_api/resources/v1/__init__.py
@@ -17,9 +17,9 @@ from typing import Optional
 from flask import Flask
 
 from .account import bp as account_bp
-from .account_statements import bp as account_statements_bp
-from .account_statements_notifications import bp as account_notifications_bp
-from .account_statements_settings import bp as account_settings_bp
+from .statements import bp as statements_bp
+from .statements_notifications import bp as statements_notifications_bp
+from .statements_settings import bp as statements_settings_bp
 from .bank_accounts import bp as bank_accounts_bp
 from .code import bp as code_bp
 from .distributions import bp as distributions_bp
@@ -50,9 +50,9 @@ class V1Endpoint:  # pylint: disable=too-few-public-methods,
 
         self.app = app
         self.app.register_blueprint(account_bp)
-        self.app.register_blueprint(account_notifications_bp)
-        self.app.register_blueprint(account_settings_bp)
-        self.app.register_blueprint(account_statements_bp)
+        self.app.register_blueprint(statements_notifications_bp)
+        self.app.register_blueprint(statements_settings_bp)
+        self.app.register_blueprint(statements_bp)
         self.app.register_blueprint(bank_accounts_bp)
         self.app.register_blueprint(code_bp)
         self.app.register_blueprint(distributions_bp)
