94ad55cc0bb500b70902e67aea58aabab76efdc5
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2023-10-17 10:20:03 -0700

    PR Fixes

--- jobs/payment-jobs/tasks/unpaid_statement_notify_task.py
+++ jobs/payment-jobs/tasks/unpaid_statement_notify_task.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Task to notify user for any outstanding statement."""
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from flask import current_app
 from pay_api.models import db
@@ -23,14 +23,11 @@ from pay_api.models.statement_invoices import StatementInvoices as StatementInvo
 from pay_api.models.statement_recipients import StatementRecipients as StatementRecipientsModel
 from pay_api.models.statement_settings import StatementSettings as StatementSettingsModel
 from pay_api.services.flags import flags
-from pay_api.services.oauth_service import OAuthService
-from pay_api.utils.enums import AuthHeaderType, ContentType, InvoiceStatus, PaymentMethod, StatementFrequency
-from pay_api.utils.util import get_first_and_last_dates_of_month
+from pay_api.utils.enums import InvoiceStatus, PaymentMethod, StatementFrequency
+from pay_api.utils.util import current_local_time, get_first_and_last_dates_of_month
 from sentry_sdk import capture_message
 from sqlalchemy import func
 
-from tasks.common.enums import OrgStatus
-from utils.auth import get_token
 from utils.mailer import publish_payment_notification
 
 
@@ -46,17 +43,10 @@ class UnpaidStatementNotifyTask:
             cls._notify_for_monthly()
 
     @classmethod
-    def _get_account(cls, auth_token: str, auth_account_id: str):
-        """Return account contact by calling auth API."""
-        get_account_endpoint = current_app.config.get('AUTH_API_ENDPOINT') + f'orgs/{auth_account_id}'
-        account_response = OAuthService.get(get_account_endpoint, auth_token, AuthHeaderType.BEARER, ContentType.JSON)
-        return account_response.json()
-
-    @classmethod
     def _notify_for_monthly(cls):  # pylint: disable=too-many-locals
         """Notify for unpaid monthly statements with an amount owing."""
         # Get the latest monthly statement
-        now = datetime.now()
+        now = current_local_time()
         previous_month = now.replace(day=1) - timedelta(days=1)
 
         send_notification = False
@@ -77,14 +67,9 @@ class UnpaidStatementNotifyTask:
             statement_settings = StatementSettingsModel.find_accounts_settings_by_frequency(previous_month,
                                                                                             StatementFrequency.MONTHLY)
             auth_account_ids = [pay_account.auth_account_id for _, pay_account in statement_settings]
-            auth_token = get_token()
 
             for account_id in auth_account_ids:
                 try:
-                    account = cls._get_account(auth_token, account_id)
-                    if account['orgStatus'] != OrgStatus.ACTIVE.value:  # Skip if the account is not in good standing
-                        continue
-
                     # Get the most recent monthly statement
                     statement = cls.find_most_recent_statement(account_id, StatementFrequency.MONTHLY.value)
                     summary = cls.get_statement_owing(account_id, statement.id)
--- jobs/payment-jobs/tests/jobs/test_unpaid_statement_notify_task.py
+++ jobs/payment-jobs/tests/jobs/test_unpaid_statement_notify_task.py
@@ -26,7 +26,7 @@ from flask import Flask
 from freezegun import freeze_time
 from pay_api.models import Statement, StatementInvoices
 from pay_api.utils.enums import InvoiceStatus, PaymentMethod, StatementFrequency
-from pay_api.utils.util import get_first_and_last_dates_of_month, get_previous_month_and_year
+from pay_api.utils.util import current_local_time, get_first_and_last_dates_of_month, get_previous_month_and_year
 
 import config
 from tasks.statement_task import StatementTask
@@ -89,11 +89,11 @@ def test_send_unpaid_statement_notification(setup, session):
     assert invoice.payment_method_code == PaymentMethod.EFT.value
     assert account.payment_method == PaymentMethod.EFT.value
 
-    now = datetime.now()
+    now = current_local_time()
     _, last_day = get_first_and_last_dates_of_month(now.month, now.year)
 
     # Generate statement for previous month - freeze time to the 1st of the current month
-    with freeze_time(datetime.now().replace(day=1)):
+    with freeze_time(current_local_time().replace(day=1)):
         StatementTask.generate_statements()
 
     # Assert statements and invoice was created
@@ -109,28 +109,23 @@ def test_send_unpaid_statement_notification(setup, session):
     with app.app_context():
         # Assert notification was published to the mailer queue
         with patch('tasks.unpaid_statement_notify_task.publish_payment_notification') as mock_mailer:
-            with patch('tasks.unpaid_statement_notify_task.get_token') as mock_get_token:
-                # Freeze time to due date - trigger due notification
-                with freeze_time(last_day):
-                    mock_get_token.return_value = 'mock_token'
-                    UnpaidStatementNotifyTask.notify_unpaid_statements()
-                    mock_get_token.assert_called()
-                    mock_mailer.assert_called_with(pay_account=account,
-                                                   statement=statements[0][0],
-                                                   is_due=True,
-                                                   due_date=last_day.date(),
-                                                   emails=statement_recipient.email)
+            # Freeze time to due date - trigger due notification
+            with freeze_time(last_day):
+                UnpaidStatementNotifyTask.notify_unpaid_statements()
+                mock_mailer.assert_called_with(pay_account=account,
+                                               statement=statements[0][0],
+                                               is_due=True,
+                                               due_date=last_day.date(),
+                                               emails=statement_recipient.email)
 
-                # Freeze time to due date - trigger reminder notification
-                with freeze_time(last_day - timedelta(days=7)):
-                    mock_get_token.return_value = 'mock_token'
-                    UnpaidStatementNotifyTask.notify_unpaid_statements()
-                    mock_get_token.assert_called()
-                    mock_mailer.assert_called_with(pay_account=account,
-                                                   statement=statements[0][0],
-                                                   is_due=False,
-                                                   due_date=last_day.date(),
-                                                   emails=statement_recipient.email)
+            # Freeze time to due date - trigger reminder notification
+            with freeze_time(last_day - timedelta(days=7)):
+                UnpaidStatementNotifyTask.notify_unpaid_statements()
+                mock_mailer.assert_called_with(pay_account=account,
+                                               statement=statements[0][0],
+                                               is_due=False,
+                                               due_date=last_day.date(),
+                                               emails=statement_recipient.email)
 
 
 def test_unpaid_statement_notification_not_sent(setup, session):
@@ -139,6 +134,6 @@ def test_unpaid_statement_notification_not_sent(setup, session):
         # Assert notification was published to the mailer queue
         with patch('tasks.unpaid_statement_notify_task.publish_payment_notification') as mock_mailer:
             # Freeze time to 10th of the month - should not trigger any notification
-            with freeze_time(datetime.now().replace(day=10)):
+            with freeze_time(current_local_time().replace(day=10)):
                 UnpaidStatementNotifyTask.notify_unpaid_statements()
                 mock_mailer.assert_not_called()
