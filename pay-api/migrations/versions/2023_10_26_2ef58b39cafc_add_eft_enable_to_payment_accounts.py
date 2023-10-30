9b3b5c3ee9b3e7fb1c2cee7a5c1305f0afc55a28
Jia Xu <Jia.Xu@gov.bc.ca>
2023-10-30 09:17:59 -0700

    unit test fix

--- pay-api/migrations/versions/2023_10_26_2ef58b39cafc_add_eft_enable_to_payment_accounts.py
+++ pay-api/migrations/versions/2023_10_26_2ef58b39cafc_add_eft_enable_to_payment_accounts.py
@@ -18,8 +18,8 @@ depends_on = None
 
 def upgrade():
     op.execute("set statement_timeout=20000;")
-    op.add_column('payment_accounts', sa.Column('eft_enable', sa.Boolean(), nullable=False, default=False))
-    op.add_column('payment_accounts_version', sa.Column('eft_enable', sa.Boolean(), nullable=False, default=False))
+    op.add_column('payment_accounts', sa.Column('eft_enable', sa.Boolean(), nullable=True, default=False))
+    op.add_column('payment_accounts_version', sa.Column('eft_enable', sa.Boolean(), nullable=True, default=False))
 
 
 def downgrade():
