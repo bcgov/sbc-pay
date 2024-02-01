77662615409981b3b182a87448f415790ccb9c85
Travis Semple <travis8814@gmail.com>
2024-02-01 10:53:48 -0800

    Add in branchName

--- pay-api/src/pay_api/services/payment_account.py
+++ pay-api/src/pay_api/services/payment_account.py
@@ -434,6 +434,7 @@ class PaymentAccount():  # pylint: disable=too-many-instance-attributes, too-man
         from pay_api.factory.payment_system_factory import PaymentSystemFactory
 
         payment_account.auth_account_id = account_request.get('accountId')
+        payment_account.branch_name = account_request.get('branchName')
 
         # If the payment method is CC, set the payment_method as DIRECT_PAY
         if payment_method := get_str_by_path(account_request, 'paymentInfo/methodOfPayment'):
