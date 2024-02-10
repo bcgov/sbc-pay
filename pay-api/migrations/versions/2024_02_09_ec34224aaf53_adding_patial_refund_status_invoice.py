65c0746c32a4759883afc0aec8d47e560412046f
Jia Xu <Jia.Xu@gov.bc.ca>
2024-02-09 16:07:28 -0800

    add PARTIAL_REFUNDED status to invoice_status_code

---
+++ pay-api/migrations/versions/2024_02_09_ec34224aaf53_adding_patial_refund_status_invoice.py
@@ -0,0 +1,34 @@
+"""empty message
+
+Revision ID: ec34224aaf53
+Revises: bbd1189e4e75
+Create Date: 2024-02-09 15:34:10.429544
+
+"""
+from alembic import op
+from sqlalchemy import String
+from sqlalchemy.sql import column, table
+
+
+# revision identifiers, used by Alembic.
+revision = 'ec34224aaf53'
+down_revision = 'bbd1189e4e75'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    status_code_table = table('invoice_status_code',
+                            column('code', String),
+                            column('description', String)
+                            )
+    op.bulk_insert(
+        status_code_table,
+        [
+            {'code': 'PARTIAL_REFUNDED', 'description': 'Partial Refunded'}
+        ]
+    )
+
+def downgrade():
+    op.execute("delete from invoice_status_code where code in ('PARTIAL_REFUNDED')")
+ 
