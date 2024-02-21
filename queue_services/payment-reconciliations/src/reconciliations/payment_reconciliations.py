08722c0e06dbd11ce5e538d981c99f9ac5df3424
Rodrigo Barraza <hello@rod.dev>
2024-02-21 13:46:42 -0800

    19929 - Checking for failed payments in NSF reconciliation flow (#1426)
    * Checking for failed payments
    
    * Remove extra blank line

--- queue_services/payment-reconciliations/src/reconciliations/payment_reconciliations.py
+++ queue_services/payment-reconciliations/src/reconciliations/payment_reconciliations.py
@@ -452,6 +452,13 @@ def _process_failed_payments(row):
     inv_number = _get_row_value(row, Column.TARGET_TXN_NO)
     payment_account: PaymentAccountModel = _get_payment_account(row)
 
+    # If there is a FAILED payment record for this; it means it's a duplicate event. Ignore it.
+    payment: PaymentModel = PaymentModel.find_payment_by_invoice_number_and_status(
+        inv_number, PaymentStatus.FAILED.value
+    )
+    if payment:
+        logger.info('Ignoring duplicate NSF message for invoice : %s ', inv_number)
+        return False
     # If there is an NSF row, it means it's a duplicate NSF event. Ignore it.
     if NonSufficientFundsService.exists_for_invoice_number(inv_number):
         logger.info('Ignoring duplicate NSF event for account: %s ', payment_account.auth_account_id)
