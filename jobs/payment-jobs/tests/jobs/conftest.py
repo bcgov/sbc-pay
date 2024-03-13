a346963a92df7e05bf3e7c9bcbd648b74c05c6ef
Travis Semple <travis8814@gmail.com>
2024-03-13 15:36:44 -0700

    attempt to fix CI #999

--- jobs/payment-jobs/tests/jobs/conftest.py
+++ jobs/payment-jobs/tests/jobs/conftest.py
@@ -57,7 +57,7 @@ def db(app):  # pylint: disable=redefined-outer-name, invalid-name
     """Return a session-wide initialised database."""
     with app.app_context():
         # even though this isn't referenced directly, it sets up the internal configs that upgrade needs
-        pay_api_dir = os.path.abspath('..').replace('jobs/payment-jobs', 'pay-api')
+        pay_api_dir = os.path.abspath('../..').replace('payment-jobs', 'pay-api')
         pay_api_dir = os.path.join(pay_api_dir, 'migrations')
         if database_exists(_db.engine.url):
             drop_database(_db.engine.url)
