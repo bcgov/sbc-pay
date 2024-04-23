792b46af41f09d7c46589f1e1ee7827b48244c4c
Jia Xu <Jia.Xu@gov.bc.ca>
2024-04-23 08:41:11 -0700

    fix for code review

--- pay-queue/src/pay_queue/external/gcp_auth.py
+++ pay-queue/src/pay_queue/external/gcp_auth.py
@@ -13,20 +13,18 @@ from requests.sessions import Session
 
 
 def verify_jwt(session):
-    """Verify token is valid and check email claims."""
+    """Check token is valid with the correct audience and email claims for configured email address."""
     try:
-        # Get the Cloud Pub/Sub-generated JWT in the "Authorization" header.
         jwt_token = request.headers.get('Authorization', '').split()[1]
-        # Verify the JWT and get the claims
-        claim = id_token.verify_oauth2_token(
+        claims = id_token.verify_oauth2_token(
             jwt_token,
             Request(session=session),
             audience=current_app.config.get('PAY_SUB_AUDIENCE')
         )
         # Check if the email is verified and matches the configured email
         required_email = current_app.config.get('VERIFY_PUBSUB_EMAIL')
-        if not claim.get('email_verified') or claim.get('email') != required_email:
-            return 'Email not verified or does not match', 400
+        if not claims.get('email_verified') or claims.get('email') != required_email:
+            return 'Email not verified or does not match', 401
     except Exception as e:
         return f'Invalid token: {e}', 400
     return None
@@ -37,9 +35,8 @@ def ensure_authorized_queue_user(f):
     @functools.wraps(f)
     def decorated_function(*args, **kwargs):
         # Use CacheControl to avoid re-fetching certificates for every request.
-        if current_app.config.get('VERIFY_PUBSUB_VIA_JWT', False):
+        if current_app.config.get('VERIFY_PUBSUB_VIA_JWT', True):
             if message := verify_jwt(CacheControl(Session())):
-                print(message)
                 abort(HTTPStatus.UNAUTHORIZED)
         return f(*args, **kwargs)
     return decorated_function
