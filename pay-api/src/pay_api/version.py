79c1447d50ad8515ad761e761823dc901d6edd02
Avni Salhotra <avni.salhotra@gov.bc.ca>
2024-05-29 11:58:43 -0700

    21472 - update sbc-pay version for release 1.21.0
    Signed-off-by: Avni Salhotra <avni.salhotra@gov.bc.ca>

--- pay-api/src/pay_api/version.py
+++ pay-api/src/pay_api/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '1.20.9'  # pylint: disable=invalid-name
+__version__ = '1.21.0'  # pylint: disable=invalid-name
