c649c98edc037a422efb5d2b21f052fa17a085da
Jia Xu <Jia.Xu@gov.bc.ca>
2024-06-04 09:57:51 -0700

    add interim statement logic when change payment away from EFT

--- pay-api/tests/unit/services/test_statement.py
+++ pay-api/tests/unit/services/test_statement.py
@@ -206,6 +206,60 @@ def test_get_weekly_interim_statement(session, admin_users_mock):
     assert weekly_invoices is not None
     assert len(weekly_invoices) == 1
     assert weekly_invoices[0].invoice_id == weekly_invoice.id
+    
+    
+def test_get_interim_statement_change_away_from_eft(session, admin_users_mock):
+    """Assert that a payment method update interim statement is generated."""
+    account_create_date = datetime(2023, 10, 1, 12, 0)
+    with freeze_time(account_create_date):
+        account: PaymentAccountService = PaymentAccountService.create(
+            get_eft_enable_account_payload(payment_method=PaymentMethod.EFT.value))
+
+        assert account is not None
+        assert account.payment_method == PaymentMethod.EFT.value
+
+    # Assert that the default MONTHLY statement settings are created
+    statement_settings: StatementSettingsModel = StatementSettingsModel \
+        .find_active_settings(str(account.auth_account_id), datetime.today())
+
+    assert statement_settings is not None
+    assert statement_settings.frequency == StatementFrequency.MONTHLY.value
+    assert statement_settings.from_date == account_create_date.date()
+    assert statement_settings.to_date is None
+
+    # Setup previous payment method interim statement data
+    invoice_create_date = localize_date(datetime(2023, 10, 9, 12, 0))
+    monthly_invoice = factory_invoice(payment_account=account, created_on=invoice_create_date,
+                                      payment_method_code=PaymentMethod.EFT.value,
+                                      status_code=InvoiceStatus.CREATED.value,
+                                      total=50).save()
+
+    assert monthly_invoice is not None
+    update_date = localize_date(datetime(2023, 10, 12, 12, 0))
+    with freeze_time(update_date):
+        account = PaymentAccountService.update(account.auth_account_id,
+                                               get_premium_account_payload(payment_method=PaymentMethod.DRAWDOWN.value,
+                                                                           account_id=account.auth_account_id))
+
+        new_statement_settings: StatementSettingsModel = StatementSettingsModel \
+            .find_latest_settings(account.auth_account_id)
+
+        assert new_statement_settings is not None
+        assert new_statement_settings.id != statement_settings.id
+        assert new_statement_settings.to_date is None
+
+    # Validate interim statement has the correct invoice
+    statements = StatementModel.find_all_statements_for_account(auth_account_id=account.auth_account_id, page=1,
+                                                                limit=100)
+
+    assert statements is not None
+    assert len(statements[0]) == 1
+
+    # Validate interim invoice is correct
+    interim_invoices = StatementInvoiceModel.find_all_invoices_for_statement(statements[0][0].id)
+    assert interim_invoices is not None
+    assert len(interim_invoices) == 1
+    assert interim_invoices[0].invoice_id == monthly_invoice.id
 
 
 def test_get_monthly_interim_statement(session, admin_users_mock):
