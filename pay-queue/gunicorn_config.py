15e9de68659a8fe53b82e970547dc5ec5116b764
Travis Semple <travis8814@gmail.com>
2024-06-10 15:35:19 -0700

    Use gunicorn config

---
+++ pay-queue/gunicorn_config.py
@@ -0,0 +1,27 @@
+# Copyright © 2024 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The configuration for gunicorn, which picks up the
+   runtime options from environment variables
+"""
+
+import os
+
+# https://docs.gunicorn.org/en/stable/settings.html#workers
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))  # gunicorn default - 1
+worker_class = os.environ.get('GUNICORN_WORKER_CLASS', 'sync')  # gunicorn default - sync
+worker_connections = int(os.environ.get('GUNICORN_WORKER_CONNECIONS', '1000'))  # gunicorn default - 1000
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))  # gunicorn default - 1
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '100'))  # gunicorn default - 30
+keepalive = int(os.environ.get('GUNICORN_KEEPALIVE', '2'))  # gunicorn default - 2
+# WHEN MIGRATING TO GCP -  GUNICORN_THREADS = 8, GUNICORN_TIMEOUT = 0, GUNICORN_PROCESSES = 1
