ef9b80129617ecbfca3edb9d7290001e20534d0e
Travis Semple <travis8814@gmail.com>
2024-07-08 23:08:29 -0700

    Update CfsAccount so they have payment method

--- pay-queue/tests/integration/factory.py
+++ pay-queue/tests/integration/factory.py
@@ -133,7 +133,8 @@ def factory_create_eft_account(auth_account_id='1234', status=CfsAccountStatus.A
     account = PaymentAccount(auth_account_id=auth_account_id,
                              payment_method=PaymentMethod.EFT.value,
                              name=f'Test EFT {auth_account_id}').save()
-    CfsAccount(status=status, account_id=account.id, cfs_account=cfs_account).save()
+    CfsAccount(status=status, account_id=account.id, cfs_account=cfs_account, payment_method=PaymentMethod.EFT.value) \
+        .save()
     return account
 
 
@@ -143,7 +144,8 @@ def factory_create_online_banking_account(auth_account_id='1234', status=CfsAcco
     account = PaymentAccount(auth_account_id=auth_account_id,
                              payment_method=PaymentMethod.ONLINE_BANKING.value,
                              name=f'Test {auth_account_id}').save()
-    CfsAccount(status=status, account_id=account.id, cfs_account=cfs_account).save()
+    CfsAccount(status=status, account_id=account.id, cfs_account=cfs_account,
+               payment_method=PaymentMethod.ONLINE_BANKING.value).save()
     return account
 
 
@@ -158,6 +160,7 @@ def factory_create_pad_account(auth_account_id='1234', bank_number='001', bank_b
                cfs_party='11111',
                cfs_account=account_number,
                cfs_site='29921',
+               payment_account=PaymentMethod.PAD.value
                ).save()
     return account
 
@@ -237,7 +240,7 @@ def factory_routing_slip_account(
             paid_amount=rs.total,
             created_by='TEST')
 
-    CfsAccount(status=status, account_id=payment_account.id).save()
+    CfsAccount(status=status, account_id=payment_account.id, payment_method=PaymentMethod.INTERNAL.value).save()
 
     return payment_account
 
