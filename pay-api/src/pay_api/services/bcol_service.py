83a0964e25da88ca827d0cab2b838c4f812cadab
Travis Semple <travis8814@gmail.com>
2024-07-12 15:13:19 -0700

    Minor tweak to BCOL-API, extra slash doesn't work

--- pay-api/src/pay_api/services/bcol_service.py
+++ pay-api/src/pay_api/services/bcol_service.py
@@ -55,7 +55,7 @@ class BcolService(PaymentSystemService, OAuthService):
                                  f'Auth Account : {payment_account.auth_account_id}')
         user: UserContext = kwargs['user']
         force_non_staff_fee_code = 'force_non_staff_fee_code' in kwargs
-        pay_endpoint = current_app.config.get('BCOL_API_ENDPOINT') + '/payments'
+        pay_endpoint = current_app.config.get('BCOL_API_ENDPOINT') + 'payments'
         invoice_number = generate_transaction_number(invoice.id)
         corp_number = invoice.business_identifier or ''
         amount_excluding_txn_fees = sum(line.total for line in line_items)
