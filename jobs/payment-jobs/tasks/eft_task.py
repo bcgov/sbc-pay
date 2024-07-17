dfa0cfd6fa073b5a44c62f0e5938a77c87d26837
Travis Semple <travis8814@gmail.com>
2024-07-17 16:20:58 -0700

    revert logging, put in new column for receipt_number

--- jobs/payment-jobs/tasks/eft_task.py
+++ jobs/payment-jobs/tasks/eft_task.py
@@ -81,7 +81,7 @@ class EFTTask:  # pylint:disable=too-few-public-methods
                 invoice_reference.flush()
                 # Note: Not creating the entire EFT as a receipt because it can be mapped to multiple CFS accounts.
                 # eft_credit_invoice_links table should reflect exactly what's in CAS.
-                receipt_number = f'EFTCIL{credit_invoice_link.id}'
+                receipt_number = f'EFT{credit_invoice_link.id}'
                 CFSService.create_cfs_receipt(
                     cfs_account=cfs_account,
                     rcpt_number=receipt_number,
@@ -100,6 +100,7 @@ class EFTTask:  # pylint:disable=too-few-public-methods
                 invoice.flush()
                 # TODO ADD UNLOCK MAILER HERE.
                 credit_invoice_link.status_code = EFTCreditInvoiceStatus.COMPLETED.value
+                credit_invoice_link.receipt_number = receipt_number
                 credit_invoice_link.flush()
                 db.session.commit()
             except Exception as e:  # NOQA # pylint: disable=broad-except
@@ -121,7 +122,7 @@ class EFTTask:  # pylint:disable=too-few-public-methods
             try:
                 current_app.logger.info(f'PayAccount: {invoice.payment_account_id} Id: {credit_invoice_link.id} -'
                                         f' Invoice Id: {invoice.id} - Amount: {credit_invoice_link.amount}')
-                receipt_number = f'EFTCIL{credit_invoice_link.id}'
+                receipt_number = credit_invoice_link.receipt_number
                 invoice_reference = InvoiceReferenceModel.find_by_invoice_id_and_status(
                     invoice.id, InvoiceReferenceStatus.COMPLETED.value
                 )
