aab77028fd91563b512320ab9746d4a3cc186ec2
Travis Semple <travis8814@gmail.com>
2024-07-17 08:23:01 -0700

    Update comment

--- pay-api/src/pay_api/models/partner_disbursements.py
+++ pay-api/src/pay_api/models/partner_disbursements.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Model to track EFT GL transfers."""
+"""Model to track Partner Disbursements, need this table because invoices can be reversed and applied multiple times."""
 
 from datetime import datetime
 
