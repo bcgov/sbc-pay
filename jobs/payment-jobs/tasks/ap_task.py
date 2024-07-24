ce79ec4328c02bed1131107b169530c9a7150bf1
Travis Semple <travis8814@gmail.com>
2024-07-23 17:20:12 -0700

    fix class method

--- jobs/payment-jobs/tasks/ap_task.py
+++ jobs/payment-jobs/tasks/ap_task.py
@@ -107,8 +107,8 @@ class ApTask(CgiAP):
 
             cls._create_file_and_upload(ap_content)
 
-    @staticmethod
-    def get_invoices_for_disbursement(partner):
+    @classmethod
+    def get_invoices_for_disbursement(cls, partner):
         """Return invoices for disbursement. Used by EJV and AP."""
         disbursement_date = datetime.today() - timedelta(days=current_app.config.get('DISBURSEMENT_DELAY_IN_DAYS'))
         invoices: List[InvoiceModel] = db.session.query(InvoiceModel) \
