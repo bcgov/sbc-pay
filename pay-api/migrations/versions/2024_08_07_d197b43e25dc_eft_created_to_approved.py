6672e807ebb7c4dcd6ccf340f7caa170fe5bb49e
Travis Semple <travis8814@gmail.com>
2024-08-07 13:58:18 -0700

    Use enum in migration

--- pay-api/migrations/versions/2024_08_07_d197b43e25dc_eft_created_to_approved.py
+++ pay-api/migrations/versions/2024_08_07_d197b43e25dc_eft_created_to_approved.py
@@ -6,6 +6,7 @@ Create Date: 2024-08-07 11:49:14.975144
 
 """
 from alembic import op
+from pay_api.utils.enums import InvoiceStatus
 
 # revision identifiers, used by Alembic.
 # Note you may see foreign keys with distribution_codes_history
@@ -17,9 +18,8 @@ down_revision = '1d5b66ef7f81'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
-    op.execute("update invoices set invoice_status_code = 'APPROVED' where invoice_status_code = 'CREATED' and payment_method_code = 'EFT'")
+    op.execute(f"update invoices set invoice_status_code = '{InvoiceStatus.APPROVED.value}' where invoice_status_code = '{InvoiceStatus.CREATED.value}' and payment_method_code = 'EFT'")
 
 def downgrade():
-    op.execute("update invoices set invoice_status_code = 'CREATED' where invoice_status_code = 'APPROVED' and payment_method_code = 'EFT'")
+    op.execute(f"update invoices set invoice_status_code = '{InvoiceStatus.CREATED.value}' where invoice_status_code = '{InvoiceStatus.APPROVED.value}' and payment_method_code = 'EFT'")
