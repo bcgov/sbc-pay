421bcc470a0bcba13afb8211a187c13d9671efcb
Travis Semple <travis8814@gmail.com>
2024-09-04 10:11:29 -0700

    Move prehook-update-db.sh back, also config cloud_environment

--- pay-api/src/pay_api/config.py
+++ pay-api/src/pay_api/config.py
@@ -68,6 +68,7 @@ class _Config:  # pylint: disable=too-few-public-methods
     """Base class configuration that should set reasonable defaults for all the other configurations."""
 
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    CLOUD_ENVIRONMENT = os.getenv('CLOUD_ENVIRONMENT', 'OCP')
 
     SECRET_KEY = 'a secret'
 
