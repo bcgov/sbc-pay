83a94879085ccb87350da80011d498692d48e14d
Jia Xu <Jia.Xu@gov.bc.ca>
2024-09-12 14:21:34 -0700

    tweaks

--- jobs/payment-jobs/tasks/stale_payment_task.py
+++ jobs/payment-jobs/tasks/stale_payment_task.py
@@ -116,9 +116,8 @@ class StalePaymentTask:  # pylint: disable=too-few-public-methods
 
             except HTTPError as http_err:
                 if http_err.response is not None and http_err.response.status_code == 404:
-                    current_app.logger.info(f'Invoice not found (404) at PAYBC. Skipping invoice id: {invoice.id}')
+                    current_app.logger.error(f'Invoice not found (404) at PAYBC. Skipping invoice id: {invoice.id}')
                     continue
                 current_app.logger.error(f'HTTPError on verifying invoice {invoice.id}: {http_err}', exc_info=True)
-                raise http_err
             except Exception as err: # NOQA # pylint: disable=broad-except
                 current_app.logger.error(f'Error verifying invoice {invoice.id}: {err}', exc_info=True)
