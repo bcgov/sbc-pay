bf4e431c1aeec123f0615272d0590167a47793a6
Rodrigo Barraza <hello@rod.dev>
2024-10-01 07:30:47 -0700

    Lint fix

--- pay-api/src/pay_api/models/eft_refund.py
+++ pay-api/src/pay_api/models/eft_refund.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Model to handle EFT REFUNDS, this is picked up by the AP job to mail out."""
+from datetime import datetime, timezone
 from typing import List
 
 from sqlalchemy import ForeignKey
