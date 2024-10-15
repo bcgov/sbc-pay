c10cfea99e705b50309492a2ff2e6c62619c97cd
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2024-10-15 10:51:01 -0700

    linting

--- pay-api/src/pay_api/services/eft_refund.py
+++ pay-api/src/pay_api/services/eft_refund.py
@@ -260,8 +260,8 @@ class EFTRefund:
             cas_supplier_number=get_str_by_path(request, "casSupplierNum"),
             refund_email=get_str_by_path(request, "refundEmail"),
             comment=comment,
-            status=EFTShortnameRefundStatus.PENDING_APPROVAL.value
-        ) 
+            status=EFTShortnameRefundStatus.PENDING_APPROVAL.value,
+        )
         refund.flush()
         return refund
 
