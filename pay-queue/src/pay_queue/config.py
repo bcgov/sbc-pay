abdab23f55bf03231ae39869d5f8c0e362431ff5
Travis Semple <travis8814@gmail.com>
2024-10-16 12:30:40 -0700

    remove unused config

--- pay-queue/src/pay_queue/config.py
+++ pay-queue/src/pay_queue/config.py
@@ -111,9 +111,6 @@ class _Config:  # pylint: disable=too-few-public-methods,protected-access
     ACCOUNT_MAILER_TOPIC = os.getenv("ACCOUNT_MAILER_TOPIC", "account-mailer-dev")
     AUTH_EVENT_TOPIC = os.getenv("AUTH_EVENT_TOPIC", "auth-event-dev")
     GCP_AUTH_KEY = os.getenv("AUTHPAY_GCP_AUTH_KEY", None)
-    # If blank in PUBSUB, this should match the https endpoint the subscription is pushing to.
-    PAY_AUDIENCE_SUB = os.getenv("PAY_AUDIENCE_SUB", None)
-    VERIFY_PUBSUB_EMAILS = f'{os.getenv("AUTHPAY_SERVICE_ACCOUNT")},{os.getenv("BUSINESS_SERVICE_ACCOUNT")}'.split(",")
 
 
 class DevConfig(_Config):  # pylint: disable=too-few-public-methods
