ad92c653b1ddea05527a762e9ce0772620778dfa
Travis Semple <travis8814@gmail.com>
2024-12-08 10:15:00 -0800

    nudge

--- pay-api/src/pay_api/models/payment.py
+++ pay-api/src/pay_api/models/payment.py
@@ -431,7 +431,7 @@ class Payment(BaseModel):  # pylint: disable=too-many-instance-attributes
             payment_account_id = (
                 db.session.query(PaymentAccount.id).filter(PaymentAccount.auth_account_id == auth_account_id).scalar()
             )
-            query = query.filter(Invoice.payment_account_id == payment_account_id or -1)
+            query = query.filter(Invoice.payment_account_id == (payment_account_id or -1))
         if account_name:
             if include_joins:
                 query = query.join(PaymentAccount, PaymentAccount.id == Invoice.payment_account_id)
