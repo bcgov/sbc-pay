1fc10ff924cbf7df6f208bcf4cf760fb37ee0b24
Travis Semple <travis8814@gmail.com>
2024-12-19 14:25:12 -0800

    tweak text

--- jobs/payment-jobs/utils/google_bucket.py
+++ jobs/payment-jobs/utils/google_bucket.py
@@ -1,4 +1,4 @@
-# Copyright © 2019 Province of British Columbia
+# Copyright © 2024 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Utility functions for sFTP."""
+"""Utility functions for Google Buckets."""
 import base64
 import json
 
