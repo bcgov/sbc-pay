a9b87f42f8925659c616193837509ff4b5bff829
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2025-01-17 10:39:03 -0800

    24648-search-transactions-role-updates (#1875)

--- pay-api/src/pay_api/resources/v1/account.py
+++ pay-api/src/pay_api/resources/v1/account.py
@@ -279,7 +279,10 @@ def post_search_purchase_history(account_number: str):
         check_auth(
             business_identifier=None,
             account_id=account_number,
-            all_of_roles=[Role.EDITOR.value, Role.VIEW_ALL_TRANSACTIONS.value],
+            one_of_role_sets=[
+                [Role.EDITOR.value, Role.VIEW_ALL_TRANSACTIONS.value],
+                [Role.VIEW_ALL_TRANSACTIONS.value],
+            ],
         )
     else:
         check_auth(
