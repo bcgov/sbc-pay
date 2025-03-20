5599100720663abc85b3bf7019e3b246c7df8fc9
Travis Semple <travis8814@gmail.com>
2025-03-20 07:12:13 -0700

    Fix for when pad activation date is None

--- pay-api/src/pay_api/factory/payment_system_factory.py
+++ pay-api/src/pay_api/factory/payment_system_factory.py
@@ -106,9 +106,10 @@ class PaymentSystemFactory:  # pylint: disable=too-few-public-methods
     @staticmethod
     def _validate_and_throw_error(instance: PaymentSystemService, payment_account: PaymentAccount):
         if isinstance(instance, PadService):
-            is_in_pad_confirmation_period = payment_account.pad_activation_date.replace(
-                tzinfo=timezone.utc
-            ) > datetime.now(tz=timezone.utc)
+            is_in_pad_confirmation_period = payment_account.pad_activation_date is not None \
+                and payment_account.pad_activation_date.replace(
+                    tzinfo=timezone.utc
+                ) > datetime.now(tz=timezone.utc)
             is_cfs_account_in_pending_status = (
                 payment_account.cfs_account_status == CfsAccountStatus.PENDING_PAD_ACTIVATION.value
             )
