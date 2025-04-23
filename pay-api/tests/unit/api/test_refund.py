d026af29908e0421ef12ce6b17ea0962d117d272
Jia Xu <Jia.Xu@gov.bc.ca>
2025-04-23 14:37:29 -0700

    unit test fix

--- pay-api/tests/unit/api/test_refund.py
+++ pay-api/tests/unit/api/test_refund.py
@@ -44,7 +44,7 @@ def test_create_refund(session, client, jwt, app, monkeypatch):
     """Assert that the endpoint  returns 202."""
     token = jwt.create_jwt(get_claims(app_request=app), token_header)
     headers = {"Authorization": f"Bearer {token}", "content-type": "application/json"}
-    monkeypatch.setattr("pay_api.services.base_payment_system._send_credit_notification", lambda *args, **kwargs: None)
+    monkeypatch.setattr("pay_api.services.base_payment_system.PaymentSystemService._send_credit_notification", lambda *args, **kwargs: None)
 
     rv = client.post(
         "/api/v1/payment-requests",
