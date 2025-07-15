ecd5b0c6d31938773273f191dbf0e684e46f95ce
Travis Semple <travis8814@gmail.com>
2025-07-15 16:01:54 -0700

    remove unique application id

--- pay-api/src/pay_api/models/applied_credits.py
+++ pay-api/src/pay_api/models/applied_credits.py
@@ -58,7 +58,7 @@ class AppliedCredits(BaseModel):
     account_id = db.Column(db.Integer, ForeignKey("payment_accounts.id"), nullable=True, index=True)
     amount_applied = db.Column(db.Numeric, nullable=False)
     # External application_id that comes straight from the CSV, looks like an identifier in an external system.
-    application_id = db.Column(db.Integer, nullable=True, index=True, unique=True)
+    application_id = db.Column(db.Integer, nullable=True, index=True)
     cfs_account = db.Column(db.String(50), nullable=False, index=True)
     cfs_identifier = db.Column(db.String(50), nullable=False, index=True)
     created_on = db.Column(
