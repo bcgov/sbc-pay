210a0ea492dff0c6e58cc1f3a3b7bbaeb1068928
Jia Xu <Jia.Xu@gov.bc.ca>
2025-08-12 10:35:37 -0700

    tweaks

--- pay-queue/src/pay_queue/services/cgi_reconciliations.py
+++ pay-queue/src/pay_queue/services/cgi_reconciliations.py
@@ -321,10 +321,9 @@ def _handle_jv_disbursement_feedback(details: JVDetailsFeedback, has_errors: boo
 
         if details.is_partial_refund:
             details.partial_refund.gl_error = DisbursementStatus.ERRORED.value
-            line_item: Optional[PaymentLineItemModel] = PaymentLineItemModel.find_by_id(
+            line_items = [PaymentLineItemModel.find_by_id(
                 details.partial_refund.payment_line_item_id
-            )
-            line_items: List[PaymentLineItemModel] = [line_item] if line_item else []
+            )]
         else:
             details.invoice.disbursement_status_code = DisbursementStatus.ERRORED.value
             line_items: List[PaymentLineItemModel] = details.invoice.payment_line_items
