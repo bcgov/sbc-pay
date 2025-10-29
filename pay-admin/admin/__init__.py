969e9efa78dd44e23ce5a6c7c580089b6e8a1719
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2025-10-29 14:15:46 -0700

    debug

--- pay-admin/admin/__init__.py
+++ pay-admin/admin/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2019 Province of British Columbia
+c# Copyright © 2019 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -43,9 +43,6 @@ def create_app(run_mode=None):
     app = Flask(__name__)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
     app.config.from_object(config.CONFIGURATION[run_mode])
-    app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # Allows cross-site usage
-    app.config['SESSION_COOKIE_SECURE'] = True      # Required when SameSite=None and using HTTPS
-    app.secret_key = os.getenv("SECRET_KEY", os.urandom(24))
 
     app.logger.info("init db.")
     db.init_app(app)
@@ -66,6 +63,55 @@ def create_app(run_mode=None):
     @app.route("/")
     def index():
         return redirect("/admin/feecode/")
+
+    @app.route('/debug/flask-oidc-session')
+    def debug_flask_oidc_session():
+        """Inspect Flask session and cookies for OIDC debugging."""
+        from flask import current_app, request, session
+        cookie_name = current_app.config.get("SESSION_COOKIE_NAME", "session")
+        cookie_value = request.cookies.get(cookie_name)
+
+        debug_info = {
+            "session_keys": list(session.keys()),
+            "has__oidc_auth_state": "_oidc_auth_state" in session,
+            "has_oidc_state": "oidc_state" in session,
+            "session_cookie_name": cookie_name,
+            "session_cookie_present": cookie_value is not None,
+            "session_cookie_value_snippet": cookie_value[:30] + "..." if cookie_value else None,
+            "session_cookie_secure": current_app.config.get("SESSION_COOKIE_SECURE"),
+            "session_cookie_samesite": current_app.config.get("SESSION_COOKIE_SAMESITE"),
+        }
+
+        print("DEBUG /debug/flask-oidc-session:", debug_info)
+        return debug_info
+
+    @app.route('/debug/oauth-callback')
+    def debug_oauth_callback():
+        from flask import current_app, request, session
+        state_from_url = request.args.get('state')
+        state_from_session = session.get('oidc_state')
+
+        cookie_name = current_app.config.get("SESSION_COOKIE_NAME", "session")
+        cookie_value = request.cookies.get(cookie_name)
+
+        debug_info = {
+            "url_state": state_from_url,
+            "session_state": state_from_session,
+            "state_match": state_from_url == state_from_session,
+            "cookie_name": cookie_name,
+            "cookie_present": cookie_value is not None,
+            "cookie_value_snippet": cookie_value[:30] + "..." if cookie_value else None,
+            "session_keys": list(session.keys()),
+            "remote_addr": request.remote_addr,
+            "headers": dict(request.headers),
+        }
+
+        print("DEBUG /debug/oauth-callback:", debug_info)
+
+        if debug_info["state_match"]:
+            return {"result": "SUCCESS", **debug_info}
+        else:
+            return {"result": "FAIL", **debug_info}, 400
 
     app.logger.info("create_app is complete.")
     return app
