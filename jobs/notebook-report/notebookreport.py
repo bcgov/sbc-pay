d8bb4b6384ff2847fe458f99d863e0d642d917cf
Travis Semple <travis8814@gmail.com>
2025-11-03 07:11:48 -0800

    comment out weekly report

--- jobs/notebook-report/notebookreport.py
+++ jobs/notebook-report/notebookreport.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     temp_dir = create_temporary_directory()
     process_partner_notebooks(temp_dir)
     # Monday is 1 and Sunday is 7 - Run Pay report weekly.
-    if date.today().isoweekday() in Config.WEEKLY_REPORT_DATES:
-        execute_notebook(ReportFiles.WEEKLY_PAY.value, temp_dir)
+    # if date.today().isoweekday() in Config.WEEKLY_REPORT_DATES:
+        #execute_notebook(ReportFiles.WEEKLY_PAY.value, temp_dir)
     logging.info("Jupyter notebook report completed in: %s", datetime.now(tz=timezone.utc) - start_time)
     sys.exit()
