4b92dae6f704624c4e728a097db2330ee95e2900
Travis Semple <travis8814@gmail.com>
2025-11-27 14:44:39 -0800

    Fix CSV service for statements (cart product) (#2163)

--- pay-api/src/pay_api/utils/query_util.py
+++ pay-api/src/pay_api/utils/query_util.py
@@ -14,24 +14,12 @@
 """Utility for common query operations."""
 
 from sqlalchemy import case, func
-from sqlalchemy.sql.selectable import Join
 
 from pay_api.models import PaymentAccount as PaymentAccountModel
 
 
 class QueryUtils:
     """Used to provide common query operations."""
-
-    @staticmethod
-    def statement_has_join(stmt, target_table):
-        """Check if statement already has a join with target table."""
-        for from_obj in stmt.get_final_froms():
-            if isinstance(from_obj, Join):
-                if from_obj.right == target_table or from_obj.left == target_table:
-                    return True
-            elif from_obj == target_table:
-                return True
-        return False
 
     @staticmethod
     def add_payment_account_name_columns(query):
