337583e9565c045bec4ad2dba5ae52d4e2f19ad0
Travis Semple <travis8814@gmail.com>
2025-12-29 10:29:21 -0800

    Fix logger so it logs to console and file (#2197)

--- jobs/payment-jobs/utils/logger.py
+++ jobs/payment-jobs/utils/logger.py
@@ -11,34 +11,80 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Logger utility for console logging without StructuredLogHandler."""
+"""Logger utility for console and file logging without StructuredLogHandler."""
 
 import logging
+import os
 import sys
+from datetime import datetime
 
 
 class LoggerUtil:
-    """Utility class for creating console loggers."""
+    """Utility class for creating console and file loggers."""
 
     _loggers = {}
+
+    @classmethod
+    def _remove_structured_handlers(cls):
+        """Remove StructuredLogHandler from all loggers globally."""
+        for name in [None, "invoke_jobs", "api-exceptions"]:
+            logger = logging.getLogger(name)
+            logger.handlers = [h for h in logger.handlers if "StructuredLogHandler" not in str(type(h))]
+
+    @classmethod
+    def _setup_logger(cls, logger, logger_key: str):
+        """Configure logger with standard settings."""
+        logger.setLevel(logging.DEBUG)
+        logger.handlers.clear()
+        logger.propagate = False
+        cls._loggers[logger_key] = logger
+        return logger
 
     @classmethod
     def get_console_logger(cls, logger_name: str):
         """Get console logger without StructuredLogHandler."""
         if logger_name not in cls._loggers:
-            # Remove StructuredLogHandler from all loggers globally
-            for name in [None, "invoke_jobs", "api-exceptions"]:
-                logger = logging.getLogger(name)
-                logger.handlers = [h for h in logger.handlers if "StructuredLogHandler" not in str(type(h))]
+            cls._remove_structured_handlers()
 
             logger = logging.getLogger(logger_name)
-            logger.setLevel(logging.DEBUG)
-            logger.handlers.clear()
-            logger.propagate = False
+            cls._setup_logger(logger, logger_name)
 
             handler = logging.StreamHandler(sys.stdout)
             handler.setFormatter(logging.Formatter("%(message)s"))
             logger.addHandler(handler)
-            cls._loggers[logger_name] = logger
 
         return cls._loggers[logger_name]
+
+    @classmethod
+    def get_file_logger(cls, logger_name: str, log_file_path: str = None):
+        """Get file logger without StructuredLogHandler.
+
+        Args:
+            logger_name: Name of the logger
+            log_file_path: Path to the log file. If not provided,
+                creates a file in logs/ directory.
+        """
+        if log_file_path is None:
+            project_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+            logs_dir = os.path.join(project_root, "logs")
+            os.makedirs(logs_dir, exist_ok=True)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            log_file_path = os.path.join(logs_dir, f"{logger_name}_{timestamp}.log")
+
+        logger_key = f"{logger_name}_file_{log_file_path}"
+        if logger_key not in cls._loggers:
+            cls._remove_structured_handlers()
+
+            logger = logging.getLogger(logger_key)
+            cls._setup_logger(logger, logger_key)
+
+            handler = logging.FileHandler(log_file_path)
+            handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s - %(levelname)s - %(message)s",
+                    "%Y-%m-%d %H:%M:%S",
+                )
+            )
+            logger.addHandler(handler)
+
+        return cls._loggers[logger_key]
