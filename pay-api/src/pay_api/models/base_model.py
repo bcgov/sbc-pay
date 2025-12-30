379b731d94b1e76fb698c084177e9a762edbbf55
Travis Semple <travis8814@gmail.com>
2025-12-30 13:26:50 -0800

    missing

--- pay-api/src/pay_api/models/base_model.py
+++ pay-api/src/pay_api/models/base_model.py
@@ -84,7 +84,6 @@ class BaseModel(db.Model):
     @classmethod
     def find_by_id_for_update(cls, identifier: int) -> Self:
         """Return model by id with lock to avoid race conditions."""
-        with db.session.begin():
-            if identifier:
-                return cls.query.filter_by(id=identifier).with_for_update().one_or_none()
-            return None
+        if identifier:
+            return cls.query.filter_by(id=identifier).with_for_update().one_or_none()
+        return None
