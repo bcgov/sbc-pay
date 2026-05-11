2f27f9fb55776ce92f86d6c7ee995e0e025269ed
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2026-05-11 10:42:14 -0700

    ci fixes

--- pay-queue/src/pay_queue/config.py
+++ pay-queue/src/pay_queue/config.py
@@ -157,10 +157,13 @@ class TestConfig(_Config):  # pylint: disable=too-few-public-methods
     DB_NAME = os.getenv("DATABASE_TEST_NAME", "")
     DB_HOST = os.getenv("DATABASE_TEST_HOST", "")
     DB_PORT = os.getenv("DATABASE_TEST_PORT", "5432")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_TEST_URL",
-        default=f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}",
-    )
+    _db_test_url = os.getenv("DATABASE_TEST_URL")
+    if _db_test_url:
+        SQLALCHEMY_DATABASE_URI = _db_test_url.replace("postgresql://", "postgresql+pg8000://", 1)
+        if "+psycopg" in SQLALCHEMY_DATABASE_URI:
+            SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("+psycopg", "+pg8000")
+    else:
+        SQLALCHEMY_DATABASE_URI = f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}"
 
     USE_DOCKER_MOCK = os.getenv("USE_DOCKER_MOCK", None)
 
